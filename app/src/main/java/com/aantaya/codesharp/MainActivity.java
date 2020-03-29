711a7df096ab8aef86b261871c80a929fdd86851
aantaya1 <antayaalexander@gmail.com>
2020-03-29 00:09:43 -0400

    added settings and fixed icon color issue w dark mode

--- app/src/main/java/com/aantaya/codesharp/MainActivity.java
+++ app/src/main/java/com/aantaya/codesharp/MainActivity.java
@@ -1,12 +1,21 @@
 package com.aantaya.codesharp;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
+import com.aantaya.codesharp.ui.settings.SettingsActivity;
+import com.aantaya.codesharp.utils.ColorUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -36,6 +45,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        setIconColorsWithAppTheme(menu);
+
+        //todo: need to add stuff to support search
+        //https://developer.android.com/training/search/setup
+
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_more:
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                return true;
+            case R.id.main_menu_filter:
+                //todo: need to implement
+                Toast.makeText(this, "Need to implement 7832", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setIconColorsWithAppTheme(Menu menu){
+        MenuItem item = menu.findItem(R.id.action_more);
+        Drawable drawableWrap = DrawableCompat.wrap(item.getIcon()).mutate();
+        DrawableCompat.setTint(drawableWrap, ColorUtils.getThemeColor(this, R.attr.colorOnPrimary));
+        item.setIcon(drawableWrap);
+
+        item = menu.findItem(R.id.main_menu_search);
+        drawableWrap = DrawableCompat.wrap(item.getIcon()).mutate();
+        DrawableCompat.setTint(drawableWrap, ColorUtils.getThemeColor(this, R.attr.colorOnPrimary));
+        item.setIcon(drawableWrap);
+
+        item = menu.findItem(R.id.main_menu_filter);
+        drawableWrap = DrawableCompat.wrap(item.getIcon()).mutate();
+        DrawableCompat.setTint(drawableWrap, ColorUtils.getThemeColor(this, R.attr.colorOnPrimary));
+        item.setIcon(drawableWrap);
     }
 }
---
+++ app/src/main/java/com/aantaya/codesharp/utils/ThemeHelper.java
@@ -0,0 +1,28 @@
+package com.aantaya.codesharp.utils;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class ThemeHelper {
+
+    public static final String LIGHT_MODE = "light";
+    public static final String DARK_MODE = "dark";
+    public static final String DEFAULT_MODE = "default";
+
+    public static void applyTheme(@NonNull String themePref) {
+        switch (themePref) {
+            case LIGHT_MODE: {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            }
+            case DARK_MODE: {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            }
+            default: {//The default theme will be dark mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            }
+        }
+    }
+}
