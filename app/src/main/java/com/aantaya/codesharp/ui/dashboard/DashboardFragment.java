4ebbc9e8773b138009d4879b1717a3999219ef3f
aantaya1 <antayaalexander@gmail.com>
2020-04-04 15:57:48 -0400

    remade dashboard layout

--- app/src/main/java/com/aantaya/codesharp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/aantaya/codesharp/ui/dashboard/DashboardFragment.java
@@ -18,11 +18,10 @@ public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -30,6 +29,7 @@ public class DashboardFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
     }
 }
