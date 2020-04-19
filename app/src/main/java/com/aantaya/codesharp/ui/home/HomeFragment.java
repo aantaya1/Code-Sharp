e8200a0996fbe296e5bd59e28bfad2b8185bbf29
aantaya1 <antayaalexander@gmail.com>
2020-04-18 23:12:31 -0400

    fixed bug with search functionality

--- app/src/main/java/com/aantaya/codesharp/ui/home/HomeFragment.java
+++ app/src/main/java/com/aantaya/codesharp/ui/home/HomeFragment.java
@@ -8,12 +8,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
