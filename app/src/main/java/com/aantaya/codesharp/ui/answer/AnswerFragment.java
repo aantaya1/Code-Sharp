47913945546514b2f6db23aa7e5bdfa521a31d0f
aantaya1 <antayaalexander@gmail.com>
2020-04-26 23:04:51 -0400

    fixed permissions and error handleing

--- app/src/main/java/com/aantaya/codesharp/ui/answer/AnswerFragment.java
+++ app/src/main/java/com/aantaya/codesharp/ui/answer/AnswerFragment.java
@@ -245,6 +245,12 @@ public class AnswerFragment extends Fragment {
         });
     }
 
+public void printNumbers(int arg1){
+    for (int i=0; i<arg1; i++){
+        System.out.println("Printing number: " + i);
+    }
+}
+
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.answer_menu, menu);
